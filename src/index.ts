f58a7fd2af04df4680d4d9f34617c1b8e553946d
yingliufilms <yingliufilms@gmail.com>
2022-11-16 16:47:38 -0500

    created User Product Update UpdatePoint schemas; refactored server & index to TypeScript; connected prisma to postgres on Render; installed Prisma Client; ran prisma migrate sucess

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const app = require('./server');
+import app from './server';
 
 app.listen(3001, () => {
 	console.log('listening on 3001');
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
 	res.json({ message: 'hello' });
 });
 
-module.exports = app;
+export default app;
