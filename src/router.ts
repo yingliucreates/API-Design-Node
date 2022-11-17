b8d0f607180880bd894657b5f549f9ddbbcb3b47
yingliufilms <yingliufilms@gmail.com>
2022-11-17 17:00:48 -0500

    populated router

---
+++ src/router.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+const router = Router();
+
+router.get('/product', (req, res) => {
+	res.json({ message: 'hello' });
+});
+router.get('/product/:id', () => {});
+router.put('/product/:id', () => {});
+router.post('/product', () => {});
+router.delete('/product/:id', () => {});
+
+router.get('/update', () => {});
+router.get('/update/:id', () => {});
+router.put('/update/:id', () => {});
+router.post('/update', () => {});
+router.delete('/update/:id', () => {});
+
+router.get('/updatepoint', () => {});
+router.get('/updatepoint/:id', () => {});
+router.put('/updatepoint/:id', () => {});
+router.post('/updatepoint', () => {});
+router.delete('/updatepoint/:id', () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import router from './router';
 const app = express();
 
 app.get('/', (req, res) => {
@@ -7,5 +7,7 @@ app.get('/', (req, res) => {
 	res.status(200);
 	res.json({ message: 'hello' });
 });
+
+app.use('/api', router);
 
 export default app;
