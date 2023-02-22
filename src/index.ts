27768ec9b599ff64e4c691a9ef82b618d17ffe2a
yingliufilms <yingliufilms@gmail.com>
2023-02-22 14:47:11 -0500

    created environment configs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import app from './server';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import config from './config';
 
-app.listen(3001, () => {
-	console.log('listening on 3001');
+app.listen(config.port, () => {
+	console.log(`listening on http://localhost:${config.port}`);
 });
